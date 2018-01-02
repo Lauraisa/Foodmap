80acb8869c1d62a034a3900ae775a0ad8a8e1d41
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-01 23:53:47 -0500

    agregando jquery, css

--- js/restaurant.js
+++ js/restaurant.js
@@ -8,5 +8,40 @@ function init(){
   }; 
     map = new google.maps.Map(document.getElementById('map'),mapOption);
     var place = new google.maps.LatLng(-12.0981589,-77.036525);
-  } 
-   
+  }
+  
+$(document).ready(function () {
+  $('#selectSuccess2').on('click',function(){
+    var valor = $(this).val();
+    if (valor === 'comida mexicana') {
+     $('#box').remove().hide(); 
+     $('#container').append('<div class="col-xs-4"> + <img class= "img-responsive img" id="enchiladas" + src="../assets/img/enchiladas.jpg">')
+     .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/guacamole.jpg">')
+     .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/pozole.jpg">');
+    } 
+    else if (valor === 'comida peruana'){
+      $('#box').remove();
+      $('#container').append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/rocoto.jpg">')
+      .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/menestron.jpg">')
+      .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/huancaina.jpg">');
+    }
+    else if (valor === 'comida japonesa'){
+      $('#box').remove();
+      $('#container').append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/yakitori.jpg">')
+      .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/takoyakis.jpg">')
+      .append('<div class="col-xs-4"> + <img class= "img-responsive img" + src="../assets/img/sushi.jpg">');
+    }
+    else {
+      return $('option','#inicio');
+    }
+  });
+
+  // $('#enchiladas').on('click', function() {
+  //   if($(this).attr('src') === 'assets/img/enchiladas.jpg') {
+  //     $('#myModal').show();
+  //   }
+  // });
+
+  
+}); 
+
